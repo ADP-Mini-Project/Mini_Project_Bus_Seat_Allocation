6d92a45869e319eb27cdd99c1d69224f71a2dc85
Bava Viknesh <bavaviknesh5@gmail.com>
2021-06-30 10:04:02 +0530

    almost done with server side

---
+++ log_get.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <title>Redirect(Login)</title>
+    </head>
+    <body>
+        <?php
+        session_start();
+        include("config.php");
+
+        $db = "registration";
+        $login = mysqli_connect($server, $user, $password,$db); 
+        if (!$login){ 
+            echo "<h2>Couldn't Connect</h2>. ";
+        }
+
+        $db = "registration";
+        $emaillog = $_POST["email"];
+        $passlog = $_POST["pass"];
+
+        $find = "";
+        $sql1 = "SELECT * FROM register";
+        $check = mysqli_query($login,$sql1);
+        if($check){
+            while($find = mysqli_fetch_assoc($check)){
+                if(($emaillog == $find["email"])&&($passlog == $find["pass"])){
+                    echo "Successful";
+                    header("location: register.html");
+                    break;
+                }
+            }
+        }
+        else{
+            echo "<br>Sorry Coudn't Connect" . mysqli_error($login);
+        }
+        mysqli_close($login);
+        ?>
+    </body>
+</html>
